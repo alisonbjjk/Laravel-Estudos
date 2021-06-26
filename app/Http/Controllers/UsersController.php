2e582761ae198f6948089d81e8b855ca638747b7
alisonbjjk <alisonbjjk@gmail.com>
2021-06-26 11:30:26 -0300

    First Commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    function getData(){
+        return "Form Data will be here";
+    }
+}
